341e65c1c1132edfad98ecf32fe8356b77e8e02b
Otman Bouftoug <obouftou@e2r11p5.1337.ma>
2025-02-22 18:00:36 +0100

    f

---
+++ mand/fract_lo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract_lo.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 17:41:16 by obouftou          #+#    #+#             */
+/*   Updated: 2025/02/22 17:56:43 by obouftou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+#   define FRACT_OL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
+
+typedef struct s_fractol
+{
+    t_fractol 
+}       t_fractol;
+
+
+
+#endif
---
+++ mand/utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 17:40:10 by obouftou          #+#    #+#             */
+/*   Updated: 2025/02/22 17:44:43 by obouftou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mand/fract_lo.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	while (s1[i] || s2[i])
+	{
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
+}

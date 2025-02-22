a0dfd4b62bce60a7a9954a60193ff6c64c793257
Otman Bouftoug <obouftou@e2r9p7.1337.ma>
2025-02-22 19:06:09 +0100

    f

--- mand/fract_ol.c
+++ mand/fract_ol.c
@@ -6,7 +6,7 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 17:39:04 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/22 18:12:59 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/22 19:04:35 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 
 int main(int ac, char **av)
 {
-    t_fractol   fract;
+	t_fractol	fract;
 
-    if (ac == 2 && (!(ft_strcmp("mandelbrot", av[1]))))
-    {
-        
-    } 
-    
+	if (ac == 2 && (!(ft_strcmp("mandelbrot", av[1]))))
+	{
+		// set the utils for mandelbrot;;
+			//set the data stucrts the work must be done on;
+		fract.title = "mandelbrot";
+		
+	}
+
 }

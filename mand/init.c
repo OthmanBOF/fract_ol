dfb5a131167573aa124d56af5f99c0325928e646
Otman Bouftoug <obouftou@e2r9p6.1337.ma>
2025-02-23 19:23:15 +0000

    f

--- mand/init.c
+++ mand/init.c
@@ -6,7 +6,7 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 14:43:44 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/23 18:59:38 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/23 19:23:04 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,37 @@
 void	window_creat(t_fractol *f)
 {
 	f->mlx_win_ptr = mlx_new_window(f->mlx_init_ptr, WIDTH, HEIGHT, f->name);
-		if (!f->mlx_win_ptr)
-		{
-			mlx_destroy_window(f->mlx_init_ptr, f->mlx_win_ptr);
-			free(f->mlx_init_ptr);
-			ft_put_err("window creation error\n", 2);
-		}
+	if (!f->mlx_win_ptr)
+	{
+		mlx_destroy_window(f->mlx_init_ptr, f->mlx_win_ptr);
+		free(f->mlx_init_ptr);
+		ft_put_err("window creation error\n", 2);
+	}
 }
 
 void	img_creat(t_fractol *f)
 {
 	f->img.img_ptr = mlx_new_image(f->mlx_init_ptr, WIDTH, HEIGHT);
-		if (!f->img.img_ptr)
-		{
-			mlx_destroy_image(f->mlx_init_ptr, f->img.img_ptr);
-			free(f->mlx_init_ptr);
-			ft_put_err("img creation error\n", 2);
-		}
+	if (!f->img.img_ptr)
+	{
+		mlx_destroy_image(f->mlx_init_ptr, f->img.img_ptr);
+		free(f->mlx_init_ptr);
+		ft_put_err("img creation error\n", 2);
+	}
+	mlx_get_data_addr(f->img.pix_data, \
+		&f->img.bit_pix, &f->img.line_len, &f->img.endian);
 }
 
-void 
+void	event_init(t_fractol *f)
+{
+}
 
 void	fract_prep(t_fractol *fractol)
 {
-
 	fractol->mlx_init_ptr = mlx_init();
 	if (!fractol->mlx_init_ptr)
 		ft_put_err("connection error\n", 2);
 	window_creat(fractol);
 	img_creat(fractol);
 	event_init(fractol);
-
 }

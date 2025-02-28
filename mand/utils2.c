52f86e5f472068ce47ef9285857cffd6d1c135c2
Otman Bouftoug <obouftou@e2r11p1.1337.ma>
2025-02-28 18:06:24 +0000

    com

--- mand/utils2.c
+++ mand/utils2.c
@@ -6,7 +6,7 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 22:02:31 by obouftou          #+#    #+#             */
-/*   Updated: 2025/02/28 17:53:21 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/28 18:04:56 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,9 @@ void	fract_creat(t_fractol *f, char *str)
 		mandelbrot(f);
 	else if (!ft_strcmp(str, "julia"))
 		julia(f);
+}
+
+double	pix_cor_to_complexcor(double val, double min, double max, double range)
+{
+	retun ((val / range) * (max - min) + min);
 }
